1be76b7d3e08e1ccd401b550a056e55c0dbc7961
Sandra Wazaz <sandrawazaz@gmail.com>
2021-03-13 15:17:15 -0500

    commit tree bda870ca1c282d265d68636f30a3d57a1b049778

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solution in this file!
+// let companyName = 'Scuber'; 
+
+const companyName = 'Scuber';
+
+//var mostProfitableNeighborhood = 'Chelsea'
+
+let mostProfitableNeighborhood = 'Chelsea'
+
+let companyCeo = 'Susan Smith'
